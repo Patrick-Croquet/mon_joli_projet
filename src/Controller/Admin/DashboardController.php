baa9fd849b5e9d92058a2179dafe64ac5a1536ef
Patrick Croquet <patrick.croquet@wanadoo.fr>
2021-01-06 14:59:31 +0100

    Initial commit

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use App\Entity\Auteur;
+use App\Entity\Genre;
+use App\Entity\Editeur;
+use App\Entity\Fournisseur;
+use App\Entity\Produit;
+
+class DashboardController extends AbstractDashboardController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        return parent::index();
+    }
+
+    // public function index(): Response
+    // {
+    //     // redirect to some CRUD controller
+    //     $routeBuilder = $this->get(CrudUrlGenerator::class)->build();
+
+    //     return $this->redirect($routeBuilder->setController(OneOfYourCrudController::class)->generateUrl());
+
+    //     // you can also redirect to different pages depending on the current user
+    //     if ('jane' === $this->getUser()->getUsername()) {
+    //         return $this->redirect('...');
+    //     }
+
+    //     // you can also render some template to display a proper Dashboard
+    //     // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+    //     return $this->render('some/path/my-dashboard.html.twig');
+    // }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Tableau de bord');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        return [
+        // MenuItem::linktoDashboard('Accueil', 'fa fa-home'),
+        MenuItem::linkToUrl('Accueil', 'fa fa-home', '/'),
+        MenuItem::linkToCrud('Utilisateurs', 'fa fa-tags', User::class),
+        MenuItem::section('Mes BD'),
+            MenuItem::linkToCrud('Produits', 'fa fa-tags', Produit::class),
+            MenuItem::linkToCrud('Auteurs', 'fa fa-tags', Auteur::class),
+            MenuItem::linkToCrud('Genres', 'fa fa-tags', Genre::class),
+            MenuItem::linkToCrud('Editeurs', 'fa fa-tags', Editeur::class),
+            MenuItem::linkToCrud('Fournisseurs', 'fa fa-tags', Fournisseur::class)
+        ];
+    }
+}
